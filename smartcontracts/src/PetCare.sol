c62f3225cd36cdf04939cc61fd5aa4e5814dfca8
Albino Aveleda <albino@bino.eng.br>
2024-03-22 10:17:21 -0300

    Retirada de acentos dos comentarios

--- smartcontracts/src/PetCare.sol
+++ smartcontracts/src/PetCare.sol
@@ -15,21 +15,21 @@ contract PetCare {
         string dateAdministered;
     }
 
-    uint8 private petIdCounter = 1;                // Inicializa o contador de ID para pets
+    uint8 private petIdCounter = 1;                 // Inicializa o contador de ID para pets
     mapping(uint8 => Pet) public pets;              // Mapeia ID do pet para a estrutura Pet
     mapping(uint8 => Vaccine[]) public petVaccines; // Mapeia ID do pet para uma lista de vacinas
 
     event PetAdded(uint8 petId, string name, address owner);
     event VaccineAdded(uint8 petId, string vaccineName);
 
-    // Função para adicionar um novo pet
+    // Funcao para adicionar um novo pet
     function addPet(string memory _name, string memory _breed, uint8 _age) public {
         pets[petIdCounter] = Pet(petIdCounter, _name, _breed, _age, msg.sender);
         emit PetAdded(petIdCounter, _name, msg.sender);
-        petIdCounter++; // Incrementa o contador para garantir um ID único para o próximo pet
+        petIdCounter++; // Incrementa o contador para garantir um ID unico para o proximo pet
     }
 
-    // Função para adicionar uma vacina a um pet específico
+    // Funcao para adicionar uma vacina a um pet especifico
     function addVaccineToPet(uint8 _petId, string memory _vaccineName, string memory _dateAdministered) public {
         // Verifica se o chamador da função é o dono do pet
         require(msg.sender == pets[_petId].owner, "Apenas o dono do pet pode adicionar vacinas.");
@@ -44,14 +44,14 @@ contract PetCare {
         return uint8(petVaccines[_petId].length);
     }
 
-    // Função para obter os detalhes de uma vacina específica de um pet
+    // Funcao para obter os detalhes de uma vacina especifica de um pet
     function getVaccineDetails(uint8 _petId, uint8 _vaccineIndex) public view returns (string memory name, string memory dateAdministered) {
         require(_vaccineIndex < petVaccines[_petId].length, "Indice da vacina invalido.");
         Vaccine memory vaccine = petVaccines[_petId][_vaccineIndex];
         return (vaccine.name, vaccine.dateAdministered);
     }
 
-    // Função para obter informações de um pet específico
+    // Funcao para obter informacoes de um pet especifico
     function getPetDetails(uint8 _petId) public view returns (string memory name, string memory breed, uint8 age, address owner) {
         Pet memory pet = pets[_petId];
         return (pet.name, pet.breed, pet.age, pet.owner);
